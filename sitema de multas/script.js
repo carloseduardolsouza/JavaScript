623634d18e85d990e013f31298b65af24cf91990
Caduhh777 <97536308+carloseduardolsouza@users.noreply.github.com>
2023-07-20 14:16:42 -0300

    Update script.js

--- sitema de multas/script.js
+++ sitema de multas/script.js
@@ -5,10 +5,11 @@ function clicar() {
     var velocidadestr = document.getElementById('velocidade')
     var velocidade = Number(velocidadestr.value)
     var resultado = document.getElementById('resultado')
-    if (velocidade >= 200) {
+    if (velocidade >= 100) {
         resultado.innerHTML = (`sua velociade e de ${velocidade}km/h`)
         resultado.innerHTML += (`<p>VOCE ESTA MULTADO</p>`)
     } else {
         resultado.innerHTML = (`sua velociade e de ${velocidade}km/h`)
+        resultado.innerHTML += (`<p>VOCE <strong>NÂO</strong> EXEDEU O LIMITE DE 100km/h</p>`)
     }
 }
