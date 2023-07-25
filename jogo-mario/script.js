531b4bce5c668509114991a01b1171b374e46043
Caduhh777 <97536308+carloseduardolsouza@users.noreply.github.com>
2023-07-25 16:52:45 -0300

    update

--- jogo-mario/script.js
+++ jogo-mario/script.js
@@ -1,19 +1,30 @@
-let mario = document.querySelector('.mario')
-let pipe = document.querySelector('.pipe')
+let mario = document.querySelector('.mario-animed')
+let pipe = document.querySelector('.pipe-animed')
+let gameover = document.querySelector('.sumir')
+let novamente = document.querySelector('#novo')
+let score = document.querySelector('.pont')
+let overpontos = document.querySelector('.realpont')
 document.addEventListener('click' , jump)
+document.addEventListener('keydown' , jump)
+novamente.addEventListener('click' , novojogo)
 let loop = setInterval(() => {
-    let pipePosition = pipe.offsetLeft
+    let pipePosition = +window.getComputedStyle(pipe).left.replace('px' , '')
     let marioPosition = +window.getComputedStyle(mario).bottom.replace('px' , '')
+    if (pipePosition < 80 && pipePosition > 75) {
+        score.innerText = Number(score.innerText) + 5
+    }
     if (pipePosition <= 80 && pipePosition > 0 && marioPosition < 60) {
-        pipe.style.animation = 'nome'
-        pipe.style.left = `${pipePosition}px`
-        mario.style.animation = 'nome'
-        mario.style.bottom = `${marioPosition}px`
+        pipe.classList.remove('pipe-animed')
+        pipe.classList.add('pipe-stop')
+        /*pipe.style.left = `${pipePosition}px`
+        mario.style.bottom = `${marioPosition}px`*/
         mario.src = 'imagens/game-over.png'
-        mario.style.width = '50px'
-        mario.style.marginLeft = '30px'
+        mario.classList.remove('mario-animed')
+        mario.classList.add('mario-stop')
+        gameover.style.display = 'block'
+        overpontos.innerText = score.innerText
 
-        clearInterval(loop)
+        /*clearInterval(loop)*/
     }
 } , 10)
 
@@ -22,6 +33,16 @@ function jump() {
     setTimeout(() => {
         mario.classList.remove('jump')
     } , 700)
+}
+
+function novojogo() {
+    gameover.style.display = 'none'
+    pipe.classList.remove('pipe-stop')
+    pipe.classList.add('pipe-animed')
+    mario.classList.remove('mario-stop')
+    mario.classList.add('mario-animed')
+    mario.src = 'imagens/mario.gif'
+    score.innerHTML = 0
 }
 
 
